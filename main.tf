c7669eae38a8a1f9f5393b568950daa716f051d4
Artyom K <artyom.k.devops@posteo.net>
2025-11-06 00:02:20 +0000

    TF Output replaced with ENV & Formatting Fix

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_s3_bucket" "state" {
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.19.0"
     }
   }
